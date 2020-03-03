8eb8c7db226a19c599468c3b1749b6b8b7553d21
vibraniumforge <gregm9999@gmail.com>
2020-03-03 13:59:25 -0800

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
 import "./App.css";
-import Search from "./containers/Search.js";
+import Routes from "./routes.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./components/Home";
+import SearchTranslations from "./containers/SearchTranslations.js";
+import SearchGenders from "./containers/SearchGenders.js";
+// import SearchGenders from "./containers/SearchGenders.js";
 
 function App() {
   return (
     <div className="App">
-      <Search />
+      <Router>
+        <div>
+          <Navbar />
+          <Switch id="routes">
+            <Route exact path="/" component={Home} />
+            <Route path="/search_translations" component={SearchTranslations} />
+            <Route path="/search_genders" component={SearchGenders} />
+            {/* <Route path="/search_etymology" component={SearchEtymologies} /> */}
+            {/* <Route path="/select_amtrak_station" component={AmtrakStationSelect} />
+        <Route path="/view_user_trains" component={ViewUserTrains} />
+        <Route path="/new_user_train" component={NewTrainForm} />
+        <Route path="/edit_user_train" component={EditTrainForm} /> */}
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
