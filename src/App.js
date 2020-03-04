7d797572fba2974bd5ecfce9d23b01f49822487c
vibraniumforge <gregm9999@gmail.com>
2020-03-04 14:40:43 -0800

    removed routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import Routes from "./routes.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
