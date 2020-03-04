c0630d108c30304319644c37594751b01e0a7608
vibraniumforge <gregm9999@gmail.com>
2020-03-04 14:41:10 -0800

    refactor

--- src/components/EtysByMacrofamilyCard.js
+++ src/components/EtysByMacrofamilyCard.js
@@ -2,16 +2,19 @@ import React, { Component } from "react";
 
 class EtysByMacrofamilyCard extends Component {
   render() {
+    console.log(this.props);
     return (
-      <div className="group-etymology-result-card">
+      <div className="etymology-result-card">
+        <p>Language: {this.props.result.name}</p>
+        <p>Translation: {this.props.result.translation}</p>
+        <p>Romanization: {this.props.result.romanization}</p>
+        <p>Gender: {this.props.result.gender}</p>
+        <a href={this.props.result.link}>Wiktionary</a>
         <p>
-          <b>Languages: </b>: {this.props.result[1].join(", ")}
-        </p>
-        <p>
-          <b>Definition: </b>
-          {this.props.result[0] !== "Null"
-            ? this.props.result[0]
-            : "None Found"}
+          Etymology:{" "}
+          {this.props.result.etymology
+            ? this.props.result.etymology.slice(0, 140)
+            : "None found"}
         </p>
       </div>
     );
