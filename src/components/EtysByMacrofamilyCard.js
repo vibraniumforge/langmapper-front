ccd2f49b2ec88a0aa124841759193f4426161ca1
vibraniumforge <gregm9999@gmail.com>
2020-03-05 11:06:40 -0800

    one more prop

--- src/components/EtysByMacrofamilyCard.js
+++ src/components/EtysByMacrofamilyCard.js
@@ -6,6 +6,7 @@ class EtysByMacrofamilyCard extends Component {
     return (
       <div className="etymology-result-card">
         <p>Language: {this.props.result.name}</p>
+        <p>Word name: {this.props.result.word_name}</p>
         <p>Translation: {this.props.result.translation}</p>
         <p>Romanization: {this.props.result.romanization}</p>
         <p>Gender: {this.props.result.gender}</p>
