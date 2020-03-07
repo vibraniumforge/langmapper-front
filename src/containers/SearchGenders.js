4c13bc880d7c3b080d1db521e5ca601fcbfce956
github username <gregm9999@gmail.com>
2020-03-06 22:39:57 -0800

    refactored input to dropdown

--- src/containers/SearchGenders.js
+++ src/containers/SearchGenders.js
@@ -5,39 +5,74 @@ class SearchGenders extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formInput: "goose",
+      selectedWord: "",
       results: [],
-      searchedWord: "goose" || this.state.formInput
+      searchedWord: "",
+      allWords: []
     };
   }
 
+  componentDidMount() {
+    fetch(`http://localhost:3001/api/v1/search/all_word_names`)
+      .then(res => res.json())
+      .then(res =>
+        this.setState({
+          allWords: res.data
+        })
+      )
+      .catch(err => console.log(err));
+  }
+
   handleOnChange = e => {
-    this.setState(
-      { formInput: e.target.value, searchedWord: e.target.value },
-      () => console.log(this.state)
-    );
+    this.setState({
+      selectedWord: e.target.value
+    });
   };
 
   handleOnSubmit = e => {
     e.preventDefault();
-    fetch(`http://localhost:3001/api/v1/search/gender/${this.state.formInput}`)
+    fetch(
+      `http://localhost:3001/api/v1/search/gender/${this.state.selectedWord}`
+    )
       .then(res => res.json())
-      .then(res => this.setState({ results: res, formInput: "" }))
+      .then(res =>
+        this.setState({
+          results: res,
+          searchedWord: this.state.selectedWord,
+          selectedWord: ""
+        })
+      )
       .catch(err => console.log(err));
   };
 
   render() {
+    const allWords =
+      this.state.allWords.length > 0
+        ? this.state.allWords.map(word => {
+            return <option key={word.id}>{word.name}</option>;
+          })
+        : null;
     return (
       <>
         <form onSubmit={e => this.handleOnSubmit(e)}>
-          <input
+          {/* <input
             type="text"
             id="search"
             placeholder="Search here"
             className="input"
             onChange={e => this.handleOnChange(e)}
             value={this.state.formInput}
-          />
+          /> */}
+
+          <select
+            id="select"
+            name="selectedWord"
+            value={this.state.selectedWord}
+            onChange={this.handleOnChange}
+          >
+            <option value="">Select One Word</option>
+            {allWords}
+          </select>
           <input type="submit" value="Search" />
         </form>
         <GenderResultsContainer
