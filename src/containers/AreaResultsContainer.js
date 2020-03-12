12c05fb7392c4bd86c40236b0c4e1aa09c782e64
vibraniumforge <gregm9999@gmail.com>
2020-03-12 16:23:24 -0700

    first commit

---
+++ src/containers/AreaResultsContainer.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import ViewAllLanguagesResultCard from "../components/ViewAllLanguagesResultCard.js";
+
+class AreaResultsContainer extends Component {
+  render() {
+    const cards =
+      this.props.results.length > 0
+        ? this.props.results.map(language => {
+            return (
+              <ViewAllLanguagesResultCard
+                language={language}
+                key={language.id}
+              />
+            );
+          })
+        : null;
+
+    return (
+      <>
+        <h3>Area: {this.props.searchedArea}</h3>
+        <h3>Location: {this.props.searchedLocation}</h3>
+        {this.props.searchedWord && this.props.results.length === 0 ? (
+          <h6>No results found</h6>
+        ) : null}
+        <div id="card-container">{cards}</div>
+      </>
+    );
+  }
+}
+
+export default AreaResultsContainer;
