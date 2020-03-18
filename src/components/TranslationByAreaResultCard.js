dcedb05107be08c20820c592b1bf00ec41754b06
github username <gregm9999@gmail.com>
2020-03-17 20:10:28 -0700

    removed logs

--- src/components/TranslationByAreaResultCard.js
+++ src/components/TranslationByAreaResultCard.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 
 class TranslationByAreaResultCard extends Component {
   render() {
-    console.log(this.props);
     return (
       <div className="translation-result-card">
         <p>
--- src/components/TranslationByAreaTextResultCard.js
+++ src/components/TranslationByAreaTextResultCard.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 
 class TranslationByAreaResultCard extends Component {
   render() {
-    console.log(this.props);
-    console.log(this.props[0]);
     return (
       <div className="translation-result-card">
         <p>
