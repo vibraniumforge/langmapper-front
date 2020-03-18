d3e22bbe8291c6c2225968cee58bab5a49e3b945
github username <gregm9999@gmail.com>
2020-03-17 22:55:32 -0700

    wip

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,23 +2,27 @@ import React from "react";
 
 const url = `http://localhost:3001/api/v1`;
 
+const getLanguagesCount = async () => {
+  try {
+    let result = await fetch(`${url}/search/language_count`);
+    console.log(result);
+    // let f = result.json();
+    // console.log(f);
+    // console.log(result.data);
+    // return result.data;
+    //   return "hi";
+    //   return result.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const getTranslationsCount = () => {
-  return fetch(`${url}/search/translation_count`)
+  fetch(`${url}/search/translation_count`)
     .then(res => res.json())
     .then(res => {
       console.log(res.data);
       return res.data;
-    })
-    .catch(err => console.log(err));
-};
-
-const getLanguagesCount = () => {
-  let result;
-  fetch(`${url}/search/language_count`)
-    .then(res => res.json())
-    .then(res => {
-      result = res.data;
-      return result;
     })
     .catch(err => console.log(err));
 };
@@ -28,6 +32,7 @@ const getWordCount = () => {
   fetch(`${url}/search/word_count`)
     .then(res => res.json())
     .then(res => {
+      console.log(res.data);
       return (result = res.data);
     })
     .then(result => result)
@@ -39,9 +44,9 @@ const Home = () => {
   return (
     <>
       <h1>Welcome to LangMapper! The language research tool!</h1>
-      <h3>Currently {getWordCount()} Words</h3>
-      <h3>Currently {getLanguagesCount()} Languages</h3>
+      {/* <h3>Currently {getLanguagesCount()} Languages</h3> */}
       <h3>Currently {getTranslationsCount()} Translations</h3>
+      <h3>Currently {getWordCount()} Words</h3>
     </>
   );
 };
