63d6117c79521be61924af28b9497708e5df5466
github username <gregm9999@gmail.com>
2020-03-27 14:30:29 -0700

    refactored to use state

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,53 +2,55 @@ import React from "react";
 
 const url = `http://localhost:3001/api/v1`;
 
-const getLanguagesCount = async () => {
-  try {
-    let result = await fetch(`${url}/search/language_count`);
-    console.log(result);
-    // let f = result.json();
-    // console.log(f);
-    // console.log(result.data);
-    // return result.data;
-    //   return "hi";
-    //   return result.data;
-  } catch (err) {
-    console.log(err);
+class Home extends React.Component {
+  state = {
+    languagesCount: null,
+    translationsCount: null,
+    wordCount: null
+  };
+
+  componentDidMount() {
+    this.getLanguagesCount();
+    this.getTranslationsCount();
+    this.getWordCount();
   }
-};
 
-const getTranslationsCount = () => {
-  fetch(`${url}/search/translation_count`)
-    .then(res => res.json())
-    .then(res => {
-      console.log(res.data);
-      return res.data;
-    })
-    .catch(err => console.log(err));
-};
+  getLanguagesCount = () => {
+    fetch(`${url}/search/language_count`)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ languagesCount: res.data });
+      })
+      .catch(err => console.log(err));
+  };
 
-const getWordCount = () => {
-  let result;
-  fetch(`${url}/search/word_count`)
-    .then(res => res.json())
-    .then(res => {
-      console.log(res.data);
-      return (result = res.data);
-    })
-    .then(result => result)
-    .catch(err => console.log(err));
-  return result;
-};
+  getTranslationsCount = () => {
+    fetch(`${url}/search/translation_count`)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ translationsCount: res.data });
+      })
+      .catch(err => console.log(err));
+  };
 
-const Home = () => {
-  return (
-    <>
-      <h1>Welcome to LangMapper! The language research tool!</h1>
-      {/* <h3>Currently {getLanguagesCount()} Languages</h3> */}
-      <h3>Currently {getTranslationsCount()} Translations</h3>
-      <h3>Currently {getWordCount()} Words</h3>
-    </>
-  );
-};
+  getWordCount = () => {
+    fetch(`${url}/search/word_count`)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ wordCount: res.data });
+      })
+      .catch(err => console.log(err));
+  };
 
+  render() {
+    return (
+      <>
+        <h1>Welcome to LangMapper! The language research tool!</h1>
+        <h3>Currently {this.state.languagesCount} Languages</h3>
+        <h3>Currently {this.state.translationsCount} Translations</h3>
+        <h3>Currently {this.state.wordCount} Words</h3>
+      </>
+    );
+  }
+}
 export default Home;
