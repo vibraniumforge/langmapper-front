538377be6e7a1d0bd4483202f28ee1d8b59098b0
github username <gregm9999@gmail.com>
2020-03-28 10:36:06 -0700

    changed to table from cards

--- src/containers/TranslationsByAreaResultsContainer.js
+++ src/containers/TranslationsByAreaResultsContainer.js
@@ -3,24 +3,55 @@ import TranslationByAreaResultCard from "../components/TranslationByAreaResultCa
 
 class TranslationsByAreaResultsContainer extends Component {
   render() {
-    const cards =
+    // const cards =
+    //   this.props.results && this.props.results.length > 0
+    //     ? this.props.results.map(translation => {
+    //         return (
+    //           <TranslationByAreaResultCard
+    //             translation={translation}
+    //             key={translation.id}
+    //           />
+    //         );
+    //       })
+    //     : null;
+    const info =
       this.props.results && this.props.results.length > 0
         ? this.props.results.map(translation => {
             return (
-              <TranslationByAreaResultCard
-                translation={translation}
-                key={translation.id}
-              />
+              <tr key={translation.id}>
+                <td>{translation.name}</td>
+                <td>{translation.translation}</td>
+                <td>{translation.romanization}</td>
+                <td>{translation.gender}</td>
+                <td>
+                  {translation.macrofamily === "Indo-European"
+                    ? "I.E."
+                    : translation.macrofamily}
+                </td>
+                <td>{translation.family}</td>
+                <td>
+                  {translation.etymology ? translation.etymology : "None Found"}
+                </td>
+              </tr>
             );
           })
         : null;
 
     return (
-      <>
-        <h3>Location: {this.props.searchedLocation}</h3>
-        <h3>Word: {this.props.searchedWord}</h3>
-        <div id="card-container">{cards}</div>
-      </>
+      <table>
+        <thead>
+          <tr>
+            <th>Language</th>
+            <th>Translation</th>
+            <th>Romanization</th>
+            <th>Gender</th>
+            <th>Macrofamily</th>
+            <th>Family</th>
+            <th>Etymology</th>
+          </tr>
+        </thead>
+        <tbody>{info}</tbody>
+      </table>
     );
   }
 }
