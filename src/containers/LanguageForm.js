2918b6ff6048051afb0dbf01cb7f68b0c1f34424
github username <gregm9999@gmail.com>
2020-03-28 10:38:06 -0700

    added edit more

--- src/containers/LanguageForm.js
+++ src/containers/LanguageForm.js
@@ -4,26 +4,62 @@ class LanguageForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "abc",
-      abbreviation: "abc",
-      alphabet: "Latn",
-      macrofamily: "Uralic",
-      family: "Finnic",
-      subfamily: "Fake here",
-      area1: "Europe",
-      area2: "North Europe",
-      area3: "Finland",
-      notes: "Note: this is fake.",
-      alive: "t",
+      //   name: "abc",
+      //   abbreviation: "abc",
+      //   alphabet: "Latn",
+      //   macrofamily: "Uralic",
+      //   family: "Finnic",
+      //   subfamily: "Fake here",
+      //   area1: "Europe",
+      //   area2: "North Europe",
+      //   area3: "Finland",
+      //   notes: "Note: this is fake.",
+      //   alive: "t",
+      name: "",
+      abbreviation: "",
+      alphabet: "",
+      macrofamily: "",
+      family: "",
+      subfamily: "",
+      area1: "",
+      area2: "",
+      area3: "",
+      notes: "",
+      alive: "",
       alphabets: [],
-      macrofamilies: []
+      macrofamilies: [],
+      editMode: null
     };
   }
 
   componentDidMount() {
+    console.log("componentDidMount fires");
+    console.log("this.props.location.pathname=", this.props.location.pathname);
     this.getAllAlphabets();
     this.getAllMacrofamilies();
+    this.setEditMode();
+    this.getLanguageById();
   }
+
+  //   componentDidUpdate() {
+  //     console.log("componentDidUpdate fires");
+  //     console.log("this.props.location.pathname=", this.props.location.pathname);
+  //     // this.setEditMode();
+  //   }
+
+  setEditMode = () => {
+    console.log("setEditMode fires");
+    console.log("this.props.location.pathname=", this.props.location.pathname);
+    if (this.props.location.pathname === "/new_language") {
+      this.setState({ editMode: false }, () =>
+        console.log("Edit Mode=", this.state.editMode)
+      );
+    } else {
+      this.setState({ editMode: true }, () =>
+        console.log("Edit Mode=", this.state.editMode)
+      );
+    }
+  };
 
   getAllAlphabets = () => {
     fetch(`http://localhost:3001/api/v1/search/all_macrofamily_names`)
@@ -47,10 +83,42 @@ class LanguageForm extends Component {
       .catch(err => console.log(err));
   };
 
+  getLanguageById = () => {
+    if (this.props.location.pathname === "/new_language") {
+      return;
+    }
+    const splitLang = this.props.location.pathname.split("/");
+    const languageId = splitLang[splitLang.length - 1];
+    return fetch(`http://localhost:3001/api/v1/languages/${languageId}`)
+      .then(res => res.json())
+      .then(res => {
+        console.log("RESULT=", res);
+        return res;
+      })
+      .then(res => {
+        this.setState(
+          {
+            name: res.name,
+            abbreviation: res.abbreviation,
+            alphabet: res.alphabet,
+            macrofamily: res.macrofamily,
+            family: res.family,
+            subfamily: res.subfamily,
+            area1: res.area1,
+            area2: res.area2,
+            area3: res.area3,
+            notes: res.notes,
+            alive: res.alive
+          },
+          () => console.log(this.state)
+        );
+      })
+
+      .catch(err => console.log(err));
+  };
+
   handleOnChange = e => {
-    this.setState({ [e.target.name]: e.target.value }, () =>
-      console.log(this.state)
-    );
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   handleOnSubmit = e => {
@@ -62,17 +130,19 @@ class LanguageForm extends Component {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        name: this.state.name,
-        abbreviation: this.state.abbreviation,
-        alphabet: this.state.alphabet,
-        macrofamily: this.state.macrofamily,
-        family: this.state.family,
-        subfamily: this.state.subfamily,
-        area1: this.state.area1,
-        area2: this.state.area2,
-        area3: this.state.area3,
-        notes: this.state.notes,
-        alive: this.state.alive
+        language: {
+          name: this.state.name,
+          abbreviation: this.state.abbreviation,
+          alphabet: this.state.alphabet,
+          macrofamily: this.state.macrofamily,
+          family: this.state.family,
+          subfamily: this.state.subfamily,
+          area1: this.state.area1,
+          area2: this.state.area2,
+          area3: this.state.area3,
+          notes: this.state.notes,
+          alive: this.state.alive
+        }
       })
     })
       .then(res => res.json())
@@ -81,8 +151,56 @@ class LanguageForm extends Component {
         return res;
       })
       .then(this.clearForm)
+      .then(this.props.history.push("/all_languages"))
       .catch(err => console.log(err));
   };
+
+  handleOnPatch = e => {
+    e.preventDefault();
+    const splitLang = this.props.location.pathname.split("/");
+    const languageId = splitLang[splitLang.length - 1];
+    fetch(`http://localhost:3001/api/v1/languages/${languageId}`, {
+      method: "PATCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        language: {
+          name: this.state.name,
+          abbreviation: this.state.abbreviation,
+          alphabet: this.state.alphabet,
+          macrofamily: this.state.macrofamily,
+          family: this.state.family,
+          subfamily: this.state.subfamily,
+          area1: this.state.area1,
+          area2: this.state.area2,
+          area3: this.state.area3,
+          notes: this.state.notes,
+          alive:
+            this.state.alive === "t" || this.state.alive === true ? true : false
+        }
+      })
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        return res;
+      })
+      //   .then(this.updateCards(languageId, languageInfo))
+      .then(this.clearForm())
+      .then(this.props.history.push("/all_languages"))
+      .catch(err => console.log(err));
+  };
+
+  cancelFormAction = () => {
+    this.props.history.push("/all_languages");
+  };
+
+  // updateCards=(languageId, languageInfo)=> {
+  //     const
+  //     this.state.
+  // }
 
   clearForm = () => {
     this.setState({
@@ -117,8 +235,19 @@ class LanguageForm extends Component {
         : null;
     return (
       <>
-        <form id="new-lang-form" onSubmit={e => this.handleOnSubmit(e)}>
-          <h3>Create a new Language</h3>
+        <form
+          id="new-lang-form"
+          onSubmit={
+            this.state.editMode
+              ? e => this.handleOnPatch(e)
+              : e => this.handleOnSubmit(e)
+          }
+        >
+          {this.state.editMode ? (
+            <h3>Edit a Language</h3>
+          ) : (
+            <h3>Create a new Language</h3>
+          )}
           <div>
             <label htmlFor="name">Name: </label>
             <input
@@ -224,7 +353,7 @@ class LanguageForm extends Component {
                 type="radio"
                 name="alive"
                 value="t"
-                checked={this.state.alive === "t"}
+                checked={this.state.alive === "t" || this.state.alive === true}
                 onChange={this.handleOnChange}
               />
               True
@@ -234,13 +363,28 @@ class LanguageForm extends Component {
                 type="radio"
                 name="alive"
                 value="f"
-                checked={this.state.alive === "f"}
+                checked={this.state.alive === "f" || this.state.alive === false}
                 onChange={this.handleOnChange}
               />
               False
             </label>
           </div>
-          <input type="submit" value="Submit" />
+          <input
+            type="submit"
+            className={this.state.name ? "submit-btn" : "disabled"}
+            value={this.state.editMode ? "Update" : "Submit"}
+            disabled={!this.state.name}
+          />
+          <button type="button" className="clear-btn" onClick={this.clearForm}>
+            Clear Form
+          </button>
+          <button
+            type="button"
+            className="cancel-btn"
+            onClick={this.cancelFormAction}
+          >
+            Cancel
+          </button>
         </form>
       </>
     );
