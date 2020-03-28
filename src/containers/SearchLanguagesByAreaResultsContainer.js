28a20a149480112d3c7d7ca21b48c4510420e6bb
github username <gregm9999@gmail.com>
2020-03-28 16:22:33 -0700

    first commit

---
+++ src/containers/SearchLanguagesByAreaResultsContainer.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import ViewAllLanguagesResultCard from "../components/ViewAllLanguagesResultCard.js";
+
+class SearchLanguagesByAreaResultsContainer extends Component {
+  render() {
+    const cards =
+      this.props.results.length > 0
+        ? this.props.results.map(language => {
+            return (
+              <ViewAllLanguagesResultCard
+                language={language}
+                key={language.id}
+              />
+            );
+          })
+        : null;
+
+    return (
+      <>
+        <h3>Location: {this.props.searchedLocation}</h3>
+        {this.props.searchedWord && this.props.results.length === 0 ? (
+          <h6>No results found</h6>
+        ) : null}
+        <div id="card-container">{cards}</div>
+      </>
+    );
+  }
+}
+
+export default SearchLanguagesByAreaResultsContainer;
