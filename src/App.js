694b2b99249a5544bf066f1c2d0b6c9b0b882d4e
github username <gregm9999@gmail.com>
2020-03-28 16:14:38 -0700

    3 renames and 2 new routes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Navbar from "./components/Navbar";
 
 import Home from "./components/Home";
 import SearchAllTranslations from "./containers/SearchAllTranslations.js";
-import SearchGenders from "./containers/SearchGenders.js";
-import SearchEtymologies from "./containers/SearchEtymologies.js";
-import GroupEtymologies from "./containers/GroupEtymologies.js";
+import SearchAllGenders from "./containers/SearchAllGenders.js";
+import SearchEtymologiesContent from "./containers/SearchEtymologiesContent.js";
+import SearchEtymologiesGrouped from "./containers/SearchEtymologiesGrouped.js";
 import SearchTranslationsByMacrofamily from "./containers/SearchTranslationsByMacrofamily.js";
 import SearchTranslationsByLanguage from "./containers/SearchTranslationsByLanguage.js";
 import WordForm from "./containers/WordForm.js";
@@ -15,6 +15,8 @@ import LanguageForm from "./containers/LanguageForm.js";
 import ViewAllLanguages from "./containers/ViewAllLanguages.js";
 import SearchLanguagesByArea from "./containers/SearchLanguagesByArea.js";
 import SearchTranslationsByArea from "./containers/SearchTranslationsByArea.js";
+import TranslationForm from "./containers/TranslationForm.js";
+import CreateEtymologyMap from "./containers/CreateEtymologyMap.js";
 // import SearchTranslationsByAreaText from "./containers/SearchTranslationsByAreaText.js";
 
 function App() {
@@ -29,9 +31,15 @@ function App() {
               path="/search_translations"
               component={SearchAllTranslations}
             />
-            <Route path="/search_genders" component={SearchGenders} />
-            <Route path="/search_etymologies" component={SearchEtymologies} />
-            <Route path="/grouped_etymologies" component={GroupEtymologies} />
+            <Route path="/search_genders" component={SearchAllGenders} />
+            <Route
+              path="/search_etymologies"
+              component={SearchEtymologiesContent}
+            />
+            <Route
+              path="/grouped_etymologies"
+              component={SearchEtymologiesGrouped}
+            />
             <Route
               path="/all_translations_by_macrofamily"
               component={SearchTranslationsByMacrofamily}
@@ -54,6 +62,11 @@ function App() {
             <Route path="/new_language" component={LanguageForm} />
             <Route path="/edit_language" component={LanguageForm} />
             <Route path="/all_languages" component={ViewAllLanguages} />
+            <Route path="/edit_translation" component={TranslationForm} />
+            <Route
+              path="/create_etymology_map"
+              component={CreateEtymologyMap}
+            />
           </Switch>
         </div>
       </Router>
