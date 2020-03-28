ad5cd3c3b922b27eb4a0373addab9e815d83b296
github username <gregm9999@gmail.com>
2020-03-28 10:35:29 -0700

    handle async loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,31 @@ class Home extends React.Component {
   state = {
     languagesCount: null,
     translationsCount: null,
-    wordCount: null
+    wordCount: null,
+    complete: false
   };
 
   componentDidMount() {
     this.getLanguagesCount();
     this.getTranslationsCount();
     this.getWordCount();
+    Promise.all([
+      fetch(`${url}/search/language_count`),
+      fetch(`${url}/search/translation_count`),
+      fetch(`${url}/search/word_count`)
+    ])
+      .then(([res1, res2, res3]) =>
+        Promise.all([res1.json(), res2.json(), res3.json()])
+      )
+      .then(([res1, res2, res3]) => {
+        this.setState({
+          languagesCount: res1.data,
+          translationsCount: res2.data,
+          wordCount: res3.data,
+          complete: true
+        });
+      })
+      .catch(err => console.log(err));
   }
 
   getLanguagesCount = () => {
@@ -43,14 +61,14 @@ class Home extends React.Component {
   };
 
   render() {
-    return (
-      <>
+    return this.state.complete ? (
+      <div id="landing-page">
         <h1>Welcome to LangMapper! The language research tool!</h1>
-        <h3>Currently {this.state.languagesCount} Languages</h3>
-        <h3>Currently {this.state.translationsCount} Translations</h3>
-        <h3>Currently {this.state.wordCount} Words</h3>
-      </>
-    );
+        <h3>Currently {this.state.wordCount} words</h3>
+        <h3>in {this.state.languagesCount} languages</h3>
+        <h3>with {this.state.translationsCount} translations!</h3>
+      </div>
+    ) : null;
   }
 }
 export default Home;
