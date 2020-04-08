7bb68cd64ed5c226de6722d040fde83515333acf
vibraniumforge <gregm9999@gmail.com>
2020-04-07 22:27:19 -0700

    changed route

--- src/containers/CreateTranslationMap.js
+++ src/containers/CreateTranslationMap.js
@@ -2,8 +2,8 @@ import React from "react";
 import CreateTranslationMapResultsContainer from "./CreateTranslationMapResultsContainer.js";
 
 // const REACT_APP_URL = process.env.REACT_APP_URL;
-const url = "http://localhost:3001/api/v1";
-// const url = "https://secure-refuge-32252.herokuapp.com/api/v1";
+// const url = "http://localhost:3001/api/v1";
+const url = "https://secure-refuge-32252.herokuapp.com/api/v1";
 
 class CreateTranslationMap extends React.Component {
   constructor(props) {
