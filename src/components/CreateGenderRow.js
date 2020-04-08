a7a64d06fecce41f02c9cba08a26e8c5b526a598
vibraniumforge <gregm9999@gmail.com>
2020-04-07 21:02:44 -0700

    first commit

---
+++ src/components/CreateGenderRow.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class CreateGenderRow extends Component {
+  render() {
+    const { translation } = this.props;
+    return (
+      <tr key={translation.id}>
+        <td>{translation.name}</td>
+        <td>{translation.translation}</td>
+        <td>{translation.romanization}</td>
+        <td>{translation.gender}</td>
+        <td>
+          {translation.macrofamily === "Indo-European"
+            ? "I.E."
+            : translation.macrofamily}
+        </td>
+        <td>{translation.family}</td>
+        <td>{translation.etymology ? translation.etymology : "None Found"}</td>
+        <td>
+          <button
+            onClick={e => this.props.onHandleEdit(e, translation.t_id)}
+            className="table-edit-btn"
+          >
+            Edit
+          </button>
+        </td>
+      </tr>
+    );
+  }
+}
+export default CreateGenderRow;
