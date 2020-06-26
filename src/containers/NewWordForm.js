efa9fa46f889cf9b118f45b40332fbfc5f21120e
vibraniumforge <gregm9999@gmail.com>
2020-06-26 15:23:45 -0700

    added redux

--- src/containers/NewWordForm.js
+++ src/containers/NewWordForm.js
@@ -1,63 +1,28 @@
 import React, { Component } from "react";
 
-// const REACT_APP_URL = process.env.REACT_APP_URL;
-// const url = 'http://localhost:3001/api/v1'
-const url = "https://secure-refuge-32252.herokuapp.com/api/v1";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+
+import { createWord } from "../actions/wordActions.js";
 
 class NewWordForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       wordName: "",
-      allWords: [],
-      ableToSubmit: null,
     };
   }
 
-  componentDidMount() {
-    this.getAllWordNames();
-  }
-
-  getAllWordNames() {
-    fetch(`${url}/search/all_word_names`)
-      .then((res) => res.json())
-      .then((res) =>
-        this.setState({
-          allWords: res.data.map((word) => word.word_name),
-        })
-      )
-      .catch((err) => console.log(err));
-  }
-
   handleOnChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value }, () =>
-      this.ableToSubmit()
-    );
-  };
-
-  ableToSubmit = () => {
-    if (this.state.allWords.includes(this.state.wordName.toLowerCase())) {
-      this.setState({ ableToSubmit: false });
-    } else {
-      this.setState({ ableToSubmit: true });
-    }
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   handleOnSubmit = (e) => {
     e.preventDefault();
-    if (this.state.ableToSubmit) {
-      fetch(`${url}/words/`, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ word: { word_name: this.state.wordName } }),
-      })
-        .then((res) => res.json())
-        .then(this.clearForm())
-        .catch((err) => console.log(err));
-    }
+    const { wordName } = this.state;
+    this.props.createWord(wordName);
+    this.props.history.push("/all_words");
   };
 
   clearForm = () => {
@@ -74,28 +39,17 @@ class NewWordForm extends Component {
             type="text"
             name="wordName"
             placeholder="Enter word here"
-            className={this.state.ableToSubmit === false ? "input-error" : ""}
             value={this.state.wordName}
             onChange={this.handleOnChange}
           />
-          <div
-            className={
-              this.state.ableToSubmit === false
-                ? "error-message"
-                : "error-message-hidden"
-            }
-          >
-            {this.state.ableToSubmit === false ? "Word Already Exists" : ""}
-          </div>
+
           <input
             type="submit"
             value="Submit"
             className={
-              this.state.wordName && this.state.ableToSubmit
-                ? "submit-btn"
-                : "disabled"
+              this.state.wordName.length > 0 ? "submit-btn" : "disabled"
             }
-            disabled={!this.state.wordName}
+            disabled={!this.state.wordName.length === 0}
           />
         </form>
       </>
@@ -103,4 +57,8 @@ class NewWordForm extends Component {
   }
 }
 
-export default NewWordForm;
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ createWord }, dispatch);
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(NewWordForm));
