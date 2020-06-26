273c932f91a97a7cba60a0cd6937879ee085c3c9
vibraniumforge <gregm9999@gmail.com>
2020-06-26 14:25:00 -0700

    new route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,9 +35,9 @@ class Navbar extends Component {
         <NavLink activeClassName="selected" to="/search_translations_by_area">
           Search Translations by Area
         </NavLink>
-        {/* <NavLink activeClassName="selected" to="/new_word">
+        <NavLink activeClassName="selected" to="/new_word">
           Create a New Word
-        </NavLink> */}
+        </NavLink>
         <NavLink activeClassName="selected" to="/all_words">
           View all Words
         </NavLink>
