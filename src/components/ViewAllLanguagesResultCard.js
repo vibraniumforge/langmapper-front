4d2f11b622bcc2408ecfd65dbea20e4f1de8be50
vibraniumforge <gregm9999@gmail.com>
2020-06-26 17:41:35 -0700

    reafactored to functional component

--- src/components/ViewAllLanguagesResultCard.js
+++ src/components/ViewAllLanguagesResultCard.js
@@ -1,74 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ViewAllLanguagesResultCard extends Component {
-  render() {
-    return (
-      <div className="language-result-card">
-        <p>
-          <strong>ID: </strong>
-          {this.props.language.id}
-        </p>
-        <p>
-          <strong>Name: </strong>
-          {this.props.language.name}
-        </p>
-        <p>
-          <strong>Abbreviation: </strong>
-          {this.props.language.abbreviation}
-        </p>
-        <p>
-          <strong>Alphabet: </strong>
-          {this.props.language.alphabet}
-        </p>
-        <p>
-          <strong>Macrofamily: </strong>
-          {this.props.language.macrofamily}
-        </p>
-        <p>
-          <strong>Family: </strong>
-          {this.props.language.family}
-        </p>
-        <p>
-          <strong>Subfamily:</strong> {this.props.language.subfamily}
-        </p>
-        <p>
-          <strong>Area 1: </strong>
-          {this.props.language.area1}
-        </p>
-        <p>
-          <strong>Area 2: </strong>
-          {this.props.language.area2}
-        </p>
-        <p>
-          <strong>Area 3: </strong>
-          {this.props.language.area3}
-        </p>
-        <p>
-          <strong>Notes: </strong>
-          {this.props.language.notes}
-        </p>
-        <p>
-          <strong>Alive: </strong>
-          {this.props.language.alive === "f" ||
-          this.props.language.alive === false
-            ? "false"
-            : "true"}
-        </p>
-        {/* <button
-          onClick={e => this.props.onHandleEdit(e, this.props.language.id)}
-          className="card-edit-btn"
-        >
-          Edit
-        </button>
-        <button
-          onClick={e => this.props.onHandleDelete(e, this.props.language.id)}
-          className="card-delete-btn"
-        >
-          Delete
-        </button> */}
-      </div>
-    );
-  }
-}
+export const ViewAllLanguagesResultCard = (props) => {
+  return (
+    <div className="language-result-card">
+      <p>
+        <strong>ID: </strong>
+        {props.language.id}
+      </p>
+      <p>
+        <strong>Name: </strong>
+        {props.language.name}
+      </p>
+      <p>
+        <strong>Abbreviation: </strong>
+        {props.language.abbreviation}
+      </p>
+      <p>
+        <strong>Alphabet: </strong>
+        {props.language.alphabet}
+      </p>
+      <p>
+        <strong>Macrofamily: </strong>
+        {props.language.macrofamily}
+      </p>
+      <p>
+        <strong>Family: </strong>
+        {props.language.family}
+      </p>
+      <p>
+        <strong>Subfamily:</strong> {props.language.subfamily}
+      </p>
+      <p>
+        <strong>Area 1: </strong>
+        {props.language.area1}
+      </p>
+      <p>
+        <strong>Area 2: </strong>
+        {props.language.area2}
+      </p>
+      <p>
+        <strong>Area 3: </strong>
+        {props.language.area3}
+      </p>
+      <p>
+        <strong>Alive: </strong>
+        {props.language.alive === "f" || props.language.alive === false
+          ? "false"
+          : "true"}
+      </p>
+      <p>
+        <strong>Notes: </strong>
+        {props.language.notes}
+      </p>
+      <button
+        onClick={(e) => props.onHandleEdit(e, props.language.id)}
+        className="card-edit-btn"
+      >
+        Edit
+      </button>
+      <button
+        onClick={(e) => props.onHandleDelete(e, props.language.id)}
+        className="card-delete-btn"
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
 
 export default ViewAllLanguagesResultCard;
