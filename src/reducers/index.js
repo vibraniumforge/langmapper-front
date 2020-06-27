a12b3ba4847af499e746b55f10f2e905c450feff
vibraniumforge <gregm9999@gmail.com>
2020-06-27 11:27:34 -0700

    added a reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import languageReducer from "./languageReducer.js";
 import countReducer from "./countReducer.js";
 import wordReducer from "./wordReducer.js";
+import translationsReducer from "./translationsReducer.js";
 
 const rootReducer = combineReducers({
   languages: languageReducer,
   counts: countReducer,
   words: wordReducer,
+  translations: translationsReducer,
 });
 
 export default rootReducer;
