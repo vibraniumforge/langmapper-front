cd41a19afd1f03a056272f36c3047b71acf6992a
vibraniumforge <gregm9999@gmail.com>
2020-06-27 22:09:25 -0700

    cleanup

--- src/components/ViewAllWordsResultCard.js
+++ src/components/ViewAllWordsResultCard.js
@@ -31,31 +31,4 @@ export const ViewAllWordsResultCard = (props) => {
   );
 };
 
-// class ViewAllWordsResultCard extends Component {
-//   render() {
-//     return (
-//       <div className="word-result-card">
-//         <p>
-//           <strong>ID: </strong>
-//           {props.word.id}
-//         </p>
-//         <p>
-//           <strong>Name: </strong>
-//           {props.word.word_name}
-//         </p>
-//         <p>
-//           <strong>Definition: </strong>
-//           {props.word.definition}
-//         </p>
-//         {/* <button
-//           onClick={e => this.props.onHandleDelete(e, props.word.id)}
-//           className="card-delete-btn"
-//         >
-//           Delete
-//         </button> */}
-//       </div>
-//     );
-//   }
-// }
-
 export default ViewAllWordsResultCard;
