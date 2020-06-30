2710c006e186ab9c464443c9269573b675fc47c8
vibraniumforge <gregm9999@gmail.com>
2020-06-30 10:42:49 -0700

    altered redux dev tools

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import "./index.css";
 import App from "./App";
 
 import { Provider } from "react-redux";
-// import { createStore, applyMiddleware } from "redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+// import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "./reducers/index";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+// const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
-// const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 // The Redux dev tools throw an error here.
 // If the program does not work comment OUT the above line and comment IN the below line.
