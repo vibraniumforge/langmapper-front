35008ff9e19c91ddeb3b630dcd6cb0bd57465e32
vibraniumforge <gregm9999@gmail.com>
2020-07-01 13:54:15 -0700

    added redux

--- src/components/SearchEtymologiesContentResultCard.js
+++ src/components/SearchEtymologiesContentResultCard.js
@@ -1,31 +1,55 @@
-import React, { Component } from "react";
+import React from "react";
+import { etymologyFormatHelper } from "../helpers/etymologyFormatHelpler.js";
 
-class SearchEtymologiesContentResultCard extends Component {
-  render() {
-    return (
-      <div className="translation-result-card">
-        <p>
-          <strong>Word: </strong> {this.props.translation.word.word_name}
-        </p>
-        <p>
-          <strong>Language: </strong> {this.props.translation.language.name}
-        </p>
-        <p>
-          <strong>Translation: </strong> {this.props.translation.translation}
-        </p>
-        <p>
-          <strong>Romanization: </strong> {this.props.translation.romanization}
-        </p>
-        <a href={this.props.translation.link}>Wiktionary</a>
-        <p>
-          <strong>Etymology: </strong>
-          {this.props.translation.etymology
-            ? this.props.translation.etymology + "..."
-            : "None found"}
-        </p>
-      </div>
-    );
-  }
-}
+export const SearchEtymologiesContentResultCard = (props) => {
+  return (
+    <div className="translation-result-card">
+      <p>
+        <strong>Word: </strong> {props.translation.word_name}
+      </p>
+      <p>
+        <strong>Language: </strong> {props.translation.name}
+      </p>
+      <p>
+        <strong>Translation: </strong> {props.translation.translation}
+      </p>
+      <p>
+        <strong>Romanization: </strong> {props.translation.romanization}
+      </p>
+      <p>
+        <strong>Etymology: </strong>
+        {etymologyFormatHelper(props.translation.etymology)}
+      </p>
+      <p>
+        <img
+          src={require("../images/wiktionary.ico")}
+          alt="icon"
+          className="wiktionary-icon"
+        />
+        <strong>
+          <a
+            href={props.translation.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Wiktionary
+          </a>
+        </strong>
+      </p>
+      <button
+        onClick={(e) => props.onHandleEdit(e, props.translation.id)}
+        className="card-edit-btn"
+      >
+        Edit
+      </button>
+      <button
+        onClick={(e) => props.onHandleDelete(e, props.translation.id)}
+        className="card-delete-btn"
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
 
 export default SearchEtymologiesContentResultCard;
