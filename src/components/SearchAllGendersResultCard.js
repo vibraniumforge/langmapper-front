1309d2752827b91e3d9eadba5e9c658f03cde550
vibraniumforge <gregm9999@gmail.com>
2020-06-30 22:32:43 -0700

    refactored to functional component

--- src/components/SearchAllGendersResultCard.js
+++ src/components/SearchAllGendersResultCard.js
@@ -1,46 +1,73 @@
-import React, { Component } from "react";
+import React from "react";
+import { genderHelper, genderColorHelper } from "../helpers/genderHelper.js";
 
-class SearchAllGendersResultCard extends Component {
-  render() {
-    let genderClass;
-    switch (this.props.translation.gender) {
-      case "m":
-        genderClass = "male";
-        break;
-      case "f":
-        genderClass = "female";
-        break;
-      default:
-        genderClass = "neuter";
-    }
-    return (
-      <div className={`gender-result-card ${genderClass}`}>
-        <p>
-          <strong>Family: </strong> {this.props.translation.language.family}
-        </p>
-        <p>
-          <strong>Language: </strong> {this.props.translation.language.name}
-        </p>
-        <p>
-          <strong>Translation: </strong> {this.props.translation.romanization}
-        </p>
-        <p>
-          <strong>Romanization: </strong> {this.props.translation.romanization}
-        </p>
-        <p>
-          <strong>Gender: </strong>
-          {this.props.translation.gender ? (
-            <span className="">
-              {this.props.translation.gender.toUpperCase()}
-            </span>
-          ) : (
-            "None found"
-          )}
-        </p>
-        <a href={this.props.translation.link}>Wiktionary</a>
-      </div>
-    );
+export const SearchAllGendersResultCard = (props) => {
+  let genderClass;
+  switch (props.translation.gender) {
+    case "m":
+      genderClass = "male";
+      break;
+    case "f":
+      genderClass = "female";
+      break;
+    default:
+      genderClass = "neuter";
   }
-}
+  return (
+    <div className={`gender-result-card ${genderClass}`}>
+      <p>
+        <strong>Family: </strong> {props.translation.family}
+      </p>
+      <p>
+        <strong>Language: </strong> {props.translation.name}
+      </p>
+      <p>
+        <strong>Translation: </strong> {props.translation.romanization}
+      </p>
+      <p>
+        <strong>Romanization: </strong> {props.translation.romanization}
+      </p>
+      <p>
+        <strong>Gender: </strong>
+        {genderHelper(props.translation.macrofamily, props.translation.name) &&
+        props.translation.gender ? (
+          <strong>
+            <span className="">{props.translation.gender.toUpperCase()}</span>
+          </strong>
+        ) : (
+          "-"
+        )}
+      </p>
+      <p>
+        <img
+          src={require("../images/wiktionary.ico")}
+          alt="icon"
+          className="wiktionary-icon"
+        />
+        <strong>
+          <a
+            href={props.translation.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Wiktionary
+          </a>
+        </strong>
+      </p>
+      <button
+        onClick={(e) => props.onHandleEdit(e, props.translation.id)}
+        className="card-edit-btn"
+      >
+        Edit
+      </button>
+      <button
+        onClick={(e) => props.onHandleDelete(e, props.translation.id)}
+        className="card-delete-btn"
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
 
 export default SearchAllGendersResultCard;
