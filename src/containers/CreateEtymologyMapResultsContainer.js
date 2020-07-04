973360a2e015bef5adaaff00a8686a185aca4ea7
vibraniumforge <gregm9999@gmail.com>
2020-07-03 22:04:48 -0700

    added redux

--- src/containers/CreateEtymologyMapResultsContainer.js
+++ src/containers/CreateEtymologyMapResultsContainer.js
@@ -4,8 +4,9 @@ import CreateEtymologyRow from "../components/CreateEtymologyRow.js";
 class CreateEtymologyMapResultsContainer extends Component {
   render() {
     const translations =
-      this.props.results && this.props.results.length > 0
-        ? this.props.results.map((translation) => {
+      this.props.translationMapByEtymology &&
+      this.props.translationMapByEtymology.length > 0
+        ? this.props.translationMapByEtymology.map((translation) => {
             return (
               <CreateEtymologyRow
                 key={translation.t_id}
@@ -18,8 +19,6 @@ class CreateEtymologyMapResultsContainer extends Component {
 
     return (
       <>
-        {/* <h3>Location: {this.props.searchedLocation}</h3>
-        <h3>Word: {this.props.searchedWord}</h3> */}
         <table>
           <thead>
             <tr>
