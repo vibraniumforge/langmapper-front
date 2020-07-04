589b74712ec7adf63307f6a958ad4a16daba5f8b
vibraniumforge <gregm9999@gmail.com>
2020-07-03 22:07:47 -0700

    refactored to functional component

--- src/components/CreateEtymologyRow.js
+++ src/components/CreateEtymologyRow.js
@@ -1,33 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 import { genderHelper, genderColorHelper } from "../helpers/genderHelper.js";
 
-class CreateEtymologyRow extends Component {
-  render() {
-    const { translation } = this.props;
-    return (
-      <tr key={translation.id}>
-        <td>{translation.name}</td>
-        <td>
-          {translation.macrofamily === "Indo-European"
-            ? "I.E."
-            : translation.macrofamily}
-        </td>
-        <td>{translation.family}</td>
-        <td>{translation.translation}</td>
-        <td>
-          {translation.translation === translation.romanization
-            ? null
-            : translation.romanization}
-        </td>
-        <td className={genderColorHelper(translation.gender)}>
-          {genderHelper(translation.macrofamily, translation.name)
-            ? translation.gender
-            : "-"}
-        </td>
-        <td className="table-etymology">
-          {translation.etymology ? translation.etymology : "None Found"}
-        </td>
-        {/* <td>
+const CreateEtymologyRow = (props) => {
+  const { translation } = props;
+  return (
+    <tr key={translation.id}>
+      <td>{translation.name}</td>
+      <td>
+        {translation.macrofamily === "Indo-European"
+          ? "I.E."
+          : translation.macrofamily}
+      </td>
+      <td>{translation.family}</td>
+      <td>{translation.translation}</td>
+      <td>
+        {translation.translation === translation.romanization
+          ? null
+          : translation.romanization}
+      </td>
+      <td
+        className={
+          genderColorHelper(
+            translation.macrofamily,
+            translation.name,
+            translation.gender
+          ) + "-result"
+        }
+      >
+        {genderHelper(
+          translation.macrofamily,
+          translation.name,
+          translation.gender
+        )}
+      </td>
+      <td className="table-etymology">
+        {translation.etymology ? translation.etymology : "None Found"}
+      </td>
+      {/* <td>
           <button
             onClick={e => this.props.onHandleEdit(e, translation.t_id)}
             className="table-edit-btn"
@@ -35,8 +44,7 @@ class CreateEtymologyRow extends Component {
             Edit
           </button>
         </td> */}
-      </tr>
-    );
-  }
-}
+    </tr>
+  );
+};
 export default CreateEtymologyRow;
