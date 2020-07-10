395daa1d16c84d0086ece1a96736bb6ec8467d45
vibraniumforge <gregm9999@gmail.com>
2020-07-09 21:07:23 -0700

    changed a method

--- src/actions/wordActions.js
+++ src/actions/wordActions.js
@@ -16,7 +16,7 @@ export const getWordById = (id) => {
   return (dispatch) => {
     fetch(`${url}/words/${id}`)
       .then((res) => res.json())
-      .then((res) => dispatch({ type: "GET_WORD_BY_ID", payload: res }))
+      .then((res) => dispatch({ type: "GET_WORD_BY_ID", payload: res.data }))
       .catch((err) => console.log(err));
   };
 };
@@ -33,7 +33,7 @@ export const createWord = (word) => {
   return (dispatch) => {
     fetch(`${url}/words`, data)
       .then((res) => res.json())
-      .then((res) => dispatch({ type: "CREATE_WORD", payload: res }))
+      .then((res) => dispatch({ type: "CREATE_WORD", payload: res.data }))
       .catch((err) => console.log(err));
   };
 };
