e83bfc6f3f5201c536fac357ac719d3ebe6383a4
vibraniumforge <gregm9999@gmail.com>
2020-07-10 11:30:20 -0700

    added userReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import languageReducer from "./languageReducer.js";
 import wordReducer from "./wordReducer.js";
 import translationReducer from "./translationReducer.js";
+import userReducer from "./userReducer.js";
 
 const rootReducer = combineReducers({
   languages: languageReducer,
   words: wordReducer,
   translations: translationReducer,
+  users: userReducer,
 });
 
 export default rootReducer;
