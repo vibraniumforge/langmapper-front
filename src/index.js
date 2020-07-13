9bdd5d89c73e0a95e46b28f93b8225331427327c
vibraniumforge <gregm9999@gmail.com>
2020-07-13 14:50:32 -0700

    added 7 new css files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import App from "./App";
+
+import "./index.css";
+import "./css/autofill.css";
+import "./css/buttons.css";
+import "./css/cards.css";
+import "./css/gender.css";
+import "./css/navbar.css";
+import "./css/table.css";
+import "./css/translationForm.css";
 
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-// import { createStore, applyMiddleware, compose } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
@@ -18,6 +26,9 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
+// Older devtools info
+
+// import { createStore, applyMiddleware, compose } from "redux";
 // WITH CHROME
 // import { createStore, applyMiddleware, compose } from "redux";
 // const store = createStore(
