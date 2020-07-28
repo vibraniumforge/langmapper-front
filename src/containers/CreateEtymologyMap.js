cb826261920aedbf186e561506dd69d6b78767ef
vibraniumforge <gregm9999@gmail.com>
2020-07-28 12:23:12 -0700

    renamed methods

--- src/containers/CreateEtymologyMap.js
+++ src/containers/CreateEtymologyMap.js
@@ -27,7 +27,7 @@ class CreateEtymologyMap extends React.Component {
     super(props);
     this.state = {
       selectedArea: "Europe",
-      selectedWord: "cat",
+      selectedWord: "",
       searchedArea: "",
       searchedWord: "",
     };
@@ -69,7 +69,7 @@ class CreateEtymologyMap extends React.Component {
         searchedArea: this.state.selectedArea,
         searchedWord: this.state.selectedWord,
         selectedArea: "Europe",
-        selectedWord: "cat",
+        selectedWord: "",
       }),
     ]);
   };
