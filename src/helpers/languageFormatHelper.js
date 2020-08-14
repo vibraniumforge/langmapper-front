78b6d8ba588daf571c95d5d6ef6b85523ab60e37
vibraniumforge <gregm9999@gmail.com>
2020-08-14 10:36:34 -0700

    new method and families

--- src/helpers/languageFormatHelper.js
+++ src/helpers/languageFormatHelper.js
@@ -5,10 +5,30 @@ const languageFormatHelper = (macrofamily, family) => {
     newMacrofamily = "I.E.";
   } else if (macrofamily === "Afro-Asiatic") {
     newMacrofamily = "A.A.";
+  } else if (macrofamily === "Northeast Caucasian") {
+    newMacrofamily = "N.E. Caucasian";
+  } else if (macrofamily === "Northwest Caucasian") {
+    newMacrofamily = "N.W. Caucasian";
   } else {
     newMacrofamily = macrofamily;
   }
   return `${newMacrofamily} - ${newFamily}`;
 };
 
-export { languageFormatHelper };
+const languageFormatHelperCard = (macrofamily) => {
+  let newMacrofamily = "";
+  if (macrofamily === "Indo-European") {
+    newMacrofamily = "I.E.";
+  } else if (macrofamily === "Afro-Asiatic") {
+    newMacrofamily = "A.A.";
+  } else if (macrofamily === "Northeast Caucasian") {
+    newMacrofamily = "N.E. Caucasian";
+  } else if (macrofamily === "Northwest Caucasian") {
+    newMacrofamily = "N.W. Caucasian";
+  } else {
+    newMacrofamily = macrofamily;
+  }
+  return newMacrofamily;
+};
+
+export { languageFormatHelper, languageFormatHelperCard };
