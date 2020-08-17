10768b5c165b887ae732dab567fa49715d26d5a9
vibraniumforge <gregm9999@gmail.com>
2020-08-17 00:21:47 -0700

    first commit refactor to common component

---
+++ src/components/MiniTable.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const MiniTable = (props) => {
+  console.log(props);
+  return (
+    <>
+      <table id="table-title">
+        <thead>
+          <tr>
+            {props.searchedArea ? <th>Area</th> : null}
+            {props.searchedMacrofamily ? <th>Macrofamily</th> : null}
+            {props.searchedLanguage ? <th>Language</th> : null}
+            {props.searchedWord ? <th>Word</th> : null}
+            {props.wordDefinition ? <th>Definition</th> : null}
+            {props.count ? <th>Count</th> : null}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {props.searchedArea ? <td>{props.searchedArea}</td> : null}
+            {props.searchedMacrofamily ? (
+              <td>{props.searchedMacrofamily}</td>
+            ) : null}
+            {props.searchedLanguage ? <td>{props.searchedLanguage}</td> : null}
+            {props.searchedWord ? <td>{props.searchedWord}</td> : null}
+            {props.wordDefinition ? <td>{props.wordDefinition}</td> : null}
+            {props.count ? <td>{props.count}</td> : null}
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default MiniTable;
